b7901623622f88a41d420349050d52c3e70a82ac
pepperpapa <zhongxin2506@outlook.com>
2022-01-08 18:27:15 +0800

    02_鼠标点击绘制点

--- src/ClickedPoints.ts
+++ src/ClickedPoints.ts
@@ -12,8 +12,29 @@ const FSHADER_SOURCE =
   gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
 }`;
 
-const HelloPoint2 = function() {
+const g_points: Array<number> = [];
+function click(event, gl: WebGLRenderingContext, canvas: HTMLCanvasElement, a_Position: number) {
+    let x = event.clientX;
+    let y = event.clientY;
+    let rect = event.target.getBoundingClientRect();
+    let new_x = (x - (rect.left + rect.width/2)) / (rect.width/2);
+    let new_y = -(y - (rect.top + rect.height/2)) / (rect.height/2);
+    g_points.push(new_x);
+    g_points.push(new_y);
+
+    // 使用指定背景色清空canvas
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    for (let i = 0; i < g_points.length; i+=2) {
+      gl.vertexAttrib3f(a_Position, g_points[i], g_points[i+1], 0.0);    
+      // 绘制点
+      gl.drawArrays(gl.POINTS, 0, 1);
+    }    
+}
+
+const ClickedPoints = function() {
     let canvas = <HTMLCanvasElement> document.getElementById("webgl");
+
     let gl = <WebGLRenderingContext> getWebGLContext(canvas, true);
     if (!gl) {
         console.log("获取WebGL上下文失败");
@@ -38,10 +59,13 @@ const HelloPoint2 = function() {
       console.log("获取a_PointSize变量存储位置失败");
       return;
     }
-    // 将顶点坐标传递给attribute变量
-    gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
-    gl.vertexAttrib1f(a_PointSize, 350.0);
-    
+    gl.vertexAttrib1f(a_PointSize, 10.0);
+
+    // 注册鼠标点击事件
+    canvas.onmousedown = function(ev) {
+        click(ev, gl, canvas, a_Position);
+    }
+
     // 指定canvas的背景颜色
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     // 使用指定背景色清空canvas
@@ -50,6 +74,6 @@ const HelloPoint2 = function() {
     gl.drawArrays(gl.POINTS, 0, 1);
 };
 
-HelloPoint2();
+ClickedPoints();
 
 export {};
